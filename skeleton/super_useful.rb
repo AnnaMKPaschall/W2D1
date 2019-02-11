fc26a7d4c389c36db302fb0a238f5a425786a7be
AnnaMKPaschall <annamkottenstette@gmail.com>
2019-02-11 15:18:25 -0500

    Error exercise

--- skeleton/super_useful.rb
+++ skeleton/super_useful.rb
@@ -1,3 +1,28 @@
+require "byebug"
+class CoffeeError < StandardError
+  def message
+    puts "Thanks for the coffee I'll give you another try"
+  end
+end
+
+class OtherError < StandardError
+  def message
+    puts "You sink"
+  end
+end
+
+class WrongYearError < StandardError
+  def message 
+    puts "You're not my best friend."
+  end 
+end 
+
+class NoImaginaryFriendsError < StandardError
+  def message 
+    puts "Are you a ghost?"
+  end 
+end 
+
 # PHASE 2
 # class InputError < StandardError ;end
 def convert_to_int(str)
@@ -14,8 +39,10 @@ FRUITS = ["apple", "banana", "orange"]
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
-    raise StandardError 
+  elsif maybe_fruit == "coffee"
+    raise CoffeeError
+  else
+    raise OtherError
   end 
 end
 
@@ -23,16 +50,25 @@ def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
 
   puts "Feed me a fruit! (Enter the name of a fruit:)"
+  begin
   maybe_fruit = gets.chomp
   reaction(maybe_fruit) 
+  rescue CoffeeError => e
+    e.message
+     retry
+  rescue OtherError => e
+    e.message
+  end
 end  
 
 # PHASE 4
 class BestFriend
+  # attr_accessor :fav_pastime
+
   def initialize(name, yrs_known, fav_pastime)
-    @name = name
-    @yrs_known = yrs_known
-    @fav_pastime = fav_pastime
+    @name = name raise NoImaginaryFriendsError if name.length <= 0
+    @yrs_known = yrs_known raise WrongYearError if yrs_known < 5 
+    @fav_pastime = fav_pastime raise NoImaginaryFriendsError if fav_pastime.length <= 0
   end
 
   def talk_about_friendship
